0e69e6352d76fb842f537897faa144db76a7eb72
Leo Oladimu <leooladimu9@gmail.com>
2022-02-23 11:05:08 -0500

    5

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router()
 const { checkUsernameExists, validateRoleName } = require("./auth-middleware")
-const { JWT_SECRET } = require("../secrets") // use this secret!
 const bcrypt = require("bcryptjs")
 const User = require("./../users/users-model")
+const makeToken = require("./makeToken")
 
 router.post("/register", validateRoleName, async (req, res, next) => {
   let user = req.body
@@ -32,7 +32,7 @@ router.post("/register", validateRoleName, async (req, res, next) => {
 router.post("/login", checkUsernameExists, (req, res, next) => {
   let { username, password } = req.body
   if (bcrypt.compareSync(password, req.userFromDb.password)) {
-    const token = tokenBuilder(req.userFromDb)
+    const token = makeToken(req.userFromDb)
     res.status(200).json({ message: `${username} is back!`, token })
   } else {
     next({ status: 401, message: "Invalid credentials" })
---
+++ api/auth/makeToken.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+const jwt = require("jsonwebtoken")	
+const { JWT_SECRET } = require("./../secrets/")
+	
+module.exports = function makeToken(user) {
+	const payload = {
+	  subject: user.user_id,
+	  username: user.username,
+	  role_name: user.role_name
+	}
+	const options = {
+	  expiresIn: "1d"
+	}
+	
+	return jwt.sign(payload, JWT_SECRET, options)
+
+}
